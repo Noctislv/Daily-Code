b6f4c76e0c6b6cacb24684bccadb095f5f71e930
Noctis_lv <31864096+Noctislv@users.noreply.github.com>
2018-02-26 12:46:54 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-	string noun[19] = { "thicc","smexy", "scuffed", "William", "succy", "ducky", "poopy", "Shakesphere", "Jake", "Floozy", "Doozy", "Chopin", "Dinky", "Jesus", "Gucci", "*crying*", "coco", "ARRRRGH", "Depressed" };
-	string adj[19] = { "thicc","smexy", "scuffed", "William", "succy", "ducky", "poopy", "Shakesphere", "Jake", "Floozy", "Doozy", "Chopin", "Dinky", "Jesus", "Gucci", "*crying*", "coco", "ARRRRGH", "Depressed" };
-
-	char input = 'y';
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include<time.h>
 
 
-	int main() {
 
-	srand(NULL);
-
-		cout << "Do you want to know your Rap name?" << endl;
-		while (input == 'y') {
-			cin >> input;
-
-			int ad = rand() % 15;
-			int nouns = rand() % 10;
-			cout << adj[ad] << endl;
-			cout << noun[nouns] << endl;
-
-		}
-	}
-
+int SCREEN_H = 900;
+int SCREEN_W = 700;
+int main() {
+	srand(time(NULL));
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP *bit = NULL;
+	al_init();
 	
+		display = al_create_display(rand() % SCREEN_H+100, rand() % SCREEN_W+200);
+		al_init_primitives_addon();
+		al_clear_to_color(al_map_rgb(rand() % 255, rand() % 255, rand() % 255));
+	while (1==1) {
+
+		
+
+		al_draw_circle(rand() % SCREEN_W, rand() % SCREEN_H, rand() % 200, al_map_rgb(rand() % 255, rand() % 255, rand() % 255), rand() % 200);
+
+
+		al_flip_display();
+		al_rest(.01);
+	}
+}
